a4f9135908e27ddf8699c170a1f8b0b97184cb02
Android Chromium Automerger <chromium-automerger@android>
2014-09-10 03:33:03 +0000

    Update makefiles after merge of Chromium at a301aef21f9e
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: Ie5c7d9fe096f07f3f235addabcf3f0c28f93a645

--- src/translator.target.darwin-arm.mk
+++ src/translator.target.darwin-arm.mk
@@ -95,6 +95,7 @@ MY_DEFS_Debug := \
 	'-DDATA_REDUCTION_PROXY_PROBE_URL="http://check.googlezip.net/connect"' \
 	'-DDATA_REDUCTION_PROXY_WARMUP_URL="http://www.gstatic.com/generate_204"' \
 	'-DVIDEO_HOLE=1' \
+	'-DENABLE_LOAD_COMPLETION_HACKS=1' \
 	'-DANGLE_TRANSLATOR_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
@@ -201,6 +202,7 @@ MY_DEFS_Release := \
 	'-DDATA_REDUCTION_PROXY_PROBE_URL="http://check.googlezip.net/connect"' \
 	'-DDATA_REDUCTION_PROXY_WARMUP_URL="http://www.gstatic.com/generate_204"' \
 	'-DVIDEO_HOLE=1' \
+	'-DENABLE_LOAD_COMPLETION_HACKS=1' \
 	'-DANGLE_TRANSLATOR_IMPLEMENTATION' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
